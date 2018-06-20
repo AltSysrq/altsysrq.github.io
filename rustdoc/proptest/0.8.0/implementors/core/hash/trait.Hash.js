a87b1ea254d84fab8b8319bb52da19d99895e8f9
Jason Lingle <jason@lin.gl>
2018-06-20 17:48:55 -0600

    Add proptest 0.8.0 (unreased) rustdocs.

---
+++ rustdoc/proptest/0.8.0/implementors/core/hash/trait.Hash.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["proptest"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"proptest/collection/struct.SizeRange.html\" title=\"struct proptest::collection::SizeRange\">SizeRange</a>",synthetic:false,types:["proptest::collection::SizeRange"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"proptest/test_runner/struct.Reason.html\" title=\"struct proptest::test_runner::Reason\">Reason</a>",synthetic:false,types:["proptest::test_runner::reason::Reason"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"proptest/string/struct.StringParam.html\" title=\"struct proptest::string::StringParam\">StringParam</a>",synthetic:false,types:["proptest::string::StringParam"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
