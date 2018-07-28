474a04acb926d982826f8b6ce1081b615efebdd8
Jason Lingle <jason@lin.gl>
2018-07-28 11:55:13 -0600

    Add proptest 0.8.4 docs.

---
+++ rustdoc/proptest/0.8.4/implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["proptest"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"proptest/test_runner/struct.Reason.html\" title=\"struct proptest::test_runner::Reason\">Reason</a>",synthetic:false,types:["proptest::test_runner::reason::Reason"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"proptest/string/struct.StringParam.html\" title=\"struct proptest::string::StringParam\">StringParam</a>",synthetic:false,types:["proptest::string::StringParam"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
