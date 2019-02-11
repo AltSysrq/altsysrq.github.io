7e93545d5e2b1367a6404e35c2d20721af5fae56
Jason Lingle <jason@lin.gl>
2019-02-11 03:20:45 +0000

    Add proptest-nostd 0.9.1 docs.

---
+++ rustdoc/proptest-nostd/0.9.1/implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["proptest"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"proptest/test_runner/struct.PersistedSeed.html\" title=\"struct proptest::test_runner::PersistedSeed\">PersistedSeed</a>",synthetic:false,types:["proptest::test_runner::failure_persistence::PersistedSeed"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"proptest/test_runner/struct.Reason.html\" title=\"struct proptest::test_runner::Reason\">Reason</a>",synthetic:false,types:["proptest::test_runner::reason::Reason"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
