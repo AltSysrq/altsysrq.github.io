ca2623a8da3a5d8f7f3fc0c1f3d8e4dee03432eb
Jason Lingle <jason@lin.gl>
2019-04-28 22:00:00 +0000

    Add proptest-nostd 0.9.3 docs.

---
+++ rustdoc/proptest-nostd/0.9.3/implementors/core/ops/arith/trait.Add.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["proptest"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a>&lt;usize&gt; for <a class=\"struct\" href=\"proptest/collection/struct.SizeRange.html\" title=\"struct proptest::collection::SizeRange\">SizeRange</a>",synthetic:false,types:["proptest::collection::SizeRange"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
